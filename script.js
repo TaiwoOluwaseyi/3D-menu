b2ee9f70d9b3eed4d8861f99b4dba383fc98a26c
Taiwo Oluwaseyi Mabel <seyitaiwo21@gmail.com>
2023-04-20 19:42:39 +0100

    another commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const centerItem = document.querySelector('.center-item');
+const centerItem = document.querySelector(".center-item");
 const menu = document.querySelector(".menu");
 
 centerItem.addEventListener("mouseover", () => {
-    menu.classList.add("change");
+  menu.classList.add("change");
 });
 
 menu.addEventListener("mouseleave", () => {
-    menu.classList.remove("change");
+  menu.classList.remove("change");
 });
